6b980d42c0fd53073ff1102bb453432bf2f1a1af
dzdykes <dzdykes88@gmail.com>
2016-11-15 16:16:41 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Program
+{
+	public Program {
+
+        static void Main(string[] args)
+        {
+		Console.WriteLine("HelloWorld");
+		Console.Read();
+        }
+
+	}   
+    
+}
---
+++ TestApp.cs
@@ -0,0 +1,7 @@
+using System;
+
+class Program {
+	public static void Main() {
+		Console.WriteLine("Testing 1, 2, 3...");
+	}
+}
